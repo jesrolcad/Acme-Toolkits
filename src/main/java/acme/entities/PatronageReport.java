276a5156bea0a619b9383fba388c66962a009e82
alebangon <alebangon@alum.us.es>
2022-03-07 20:38:03 +0100

    Task 008: Done
    Se ha creado la entidad PatronageReport, para el sequenceNumber he creado un patrón regexp según las instrucciones, aunque las validaciones de que coincida con el patronage code y un serialNumber se harán más adelante en el Service. #43

---
+++ src/main/java/acme/entities/PatronageReport.java
@@ -0,0 +1,55 @@
+package acme.entities;
+
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Pattern(regexp = "^<[A-Z]{3}-[0-9]{3}(-[A-Z])?:[0-9]{4,6}>$")
+	@Column(unique = true)
+	protected String			sequenceNumber;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date				creationMoment;
+	
+	@NotBlank
+	@Length(max = 256)
+	protected String 			memorandum;
+	
+	@URL
+	protected String			link;
+	
+	// Derived attributes -----------------------------------------------------
+
+	
+	// Relationships ----------------------------------------------------------
+	@JoinColumn(name = "patronageId", referencedColumnName = "id")
+	protected Patronage			patronage;
+	
+}
