ed7e0ef7e34ae2349ef4d3e320c5370e7ea92786
julibamon <juliaibmontero@gmail.com>
2022-03-12 12:34:30 +0100

    Task 002: Done

---
+++ src/main/java/acme/entities/Announcement.java
@@ -0,0 +1,58 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Announcement extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				moment;
+
+	@Length(max = 101)
+	@NotBlank
+	protected String			title;
+
+	@Length(max = 101)
+	@NotBlank
+	protected String			author;
+
+	@Length(max = 256)
+	@NotBlank
+	protected String			body;
+	
+	@NotNull
+	protected Boolean			critical;
+	
+	@URL
+	protected String			info;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
---
+++ src/main/java/acme/entities/Chirp.java
@@ -0,0 +1,52 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Chirp extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date				moment;
+
+	@Length(max = 101)
+	@NotBlank
+	protected String			title;
+
+	@Length(max = 101)
+	@NotBlank
+	protected String			author;
+
+	@Length(max = 256)
+	@NotBlank
+	protected String			body;
+
+	@Email
+	protected String			email;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
