a020a96510b7b6faaf25c8a79a46ca116066bf75
jesrolcad <jesrolcad@alum.us.es>
2022-03-17 19:56:49 +0100

    Task 011: Done

---
+++ src/main/java/acme/entities/SystemConfiguration.java
@@ -0,0 +1,43 @@
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SystemConfiguration extends AbstractEntity{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	public String acceptedCurrencies;
+	
+	@NotBlank
+	public String systemCurrency;
+	
+	@NotBlank
+	public String strongSpamWords;
+	
+	@Range(min = 0, max = 100)
+	@Digits(integer = 3, fraction = 2)
+	@NotNull
+	public double strongSpamThreshold;
+	
+	@NotBlank
+	public String weakSpamWords;
+	
+	@Range(min = 0, max = 100)
+	@Digits(integer = 3, fraction = 2)
+	@NotNull
+	public double weakSpamThreshold;
+
+}
