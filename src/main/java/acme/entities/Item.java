e3461c87a950271620a941cbc70e5a179db5f360
JaviGGT <javgrogom@alum.us.es>
2022-03-19 11:14:49 +0100

    Task 006-R1
    Error solucionado

--- src/main/java/acme/entities/Item.java
+++ src/main/java/acme/entities/Item.java
@@ -1,6 +1,5 @@
 package acme.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -32,7 +31,7 @@ public class Item extends AbstractEntity {
 	@Length(max=101)
 	protected String name;
 	
-	@Column(unique = true)
+	@NotBlank
 	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	protected String code;
 	
