2a5bbe8f42483df60e12de9f1f0890bdc8a7baba
Romanflodriguez <Romanflodriguez13@gmail.com>
2022-03-19 12:14:23 +0100

    Task 010: Done

--- src/main/java/acme/forms/PatronDashboard.java
+++ src/main/java/acme/forms/PatronDashboard.java
@@ -18,20 +18,11 @@ public class PatronDashboard implements Serializable {
 
 	// Attributes -------------------------------------------------------------
 
-	Map<String, Integer> totalNumberItems;
-	Map<Pair<String,String>,Double>averageRetailPriceOfComponents;
-	Map<Pair<String,String>,Double>deviationRetailPriceOfComponents;
-	Map<Pair<String,String>,Double>minRetailPriceOfComponents;
-	Map<Pair<String,String>,Double>maxRetailPriceOfComponents;
-	Map<String,Double> averageRetailPriceOfTools;
-	Map<String,Double> deviationRetailPriceOfTools;
-	Map<String,Double> maxRetailPriceOfTools;
-	Map<String,Double> minRetailPriceOfTools;
 	Map<String,Integer> totalNumberPatronagesByStatus;
-	Map<String,Double> averageBudgetByCurrency;
-	Map<String,Double> deviationBudgetByCurrency;
-	Map<String,Double> minBudgetByCurrency;
-	Map<String,Double> maxBudgetByCurrency;
+    Map<Pair<String,String>,Double> averageBudgetByCurrency;
+    Map<Pair<String,String>,Double> deviationBudgetByCurrency;
+    Map<Pair<String,String>,Double> minBudgetByCurrency;
+    Map<Pair<String,String>,Double> maxBudgetByCurrency;
 
 
 
