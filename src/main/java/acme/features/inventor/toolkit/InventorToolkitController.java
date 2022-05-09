41bef171b491e3d2857a44249ddb6d3f27545f60
JaviGGT <javgrogom@alum.us.es>
2022-05-09 13:34:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/acme/features/inventor/toolkit/InventorToolkitController.java
+++ src/main/java/acme/features/inventor/toolkit/InventorToolkitController.java
@@ -20,9 +20,28 @@ public class InventorToolkitController extends AbstractController<Inventor, Tool
 			@Autowired
 			protected InventorToolkitShowService	showService;
 			
+			@Autowired
+			protected InventorToolkitCreateService	createService;
+			
+//			@Autowired
+//			protected inventortoolkitupdateservice	updateservice;
+			
+			@Autowired
+			protected InventorToolkitDeleteService	deleteService;
+			
+//			@Autowired
+//			protected InventorToolkitPublishService	publishService;
+			
 			@PostConstruct
 			protected void initialise() {
+				super.addCommand("create", this.createService);
+//				super.addCommand("update", this.updateService);
+				super.addCommand("delete", this.deleteService);
+//				super.addCommand("publish","update", this.publishService);
+				
 				super.addCommand("list", this.toolkitListMineService);
 				super.addCommand("show", this.showService);
+				
+				
 			}
 }
