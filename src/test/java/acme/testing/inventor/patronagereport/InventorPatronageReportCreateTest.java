cc063f4720866adf4e13f9ce05a3e59a317f1e6a
romflorod <romflorod@alum.us.es>
2022-05-15 00:22:01 +0200

    Task 008 -T : Done

--- src/test/java/acme/testing/inventor/patronagereport/InventorPatronageReportCreateTest.java
+++ src/test/java/acme/testing/inventor/patronagereport/InventorPatronageReportCreateTest.java
@@ -10,7 +10,7 @@ public class InventorPatronageReportCreateTest extends TestHarness{
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/patronageReport/createPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(2)
 	public void positivePatronage(final int recordIndex, final String memorandum,final String link) {
 		
 		
@@ -25,8 +25,35 @@ public class InventorPatronageReportCreateTest extends TestHarness{
 		super.fillInputBoxIn("confirmation", "true");
 		super.clickOnSubmit("Create");
 		super.clickOnMenu("Inventor", "Patronage reports");
-		super.clickOnListingRecord(2);
+		super.clickOnListingRecord(3);
 		super.checkInputBoxHasValue("memorandum", memorandum);
+		super.checkInputBoxHasValue("link", link);
+		
+
+
+		
+
+		super.signOut();
+	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/patronageReport/createPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(1)
+	public void positivePatronage255chars(final int recordIndex, final String memorandum,final String link) {
+		
+		
+		super.signIn("inventor1", "inventor1");
+	
+		super.clickOnMenu("Inventor", "List patronages");
+		//show
+		super.clickOnListingRecord(recordIndex);
+		super.clickOnButton("Create Report");
+		super.fillInputBoxIn("memorandum", "Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non");
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("confirmation", "true");
+		super.clickOnSubmit("Create");
+		super.clickOnMenu("Inventor", "Patronage reports");
+		super.clickOnListingRecord(2);
+		super.checkInputBoxHasValue("memorandum", "Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non");
 		super.checkInputBoxHasValue("link", link);
 		
 
@@ -89,7 +116,23 @@ public void negativePatronage3(final int recordIndex, final String memorandum,fi
 		super.clickOnSubmit("Create");
 		super.checkErrorsExist();
 	}
-
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/patronageReport/createNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+public void negativePatronage256chars(final int recordIndex, final String memorandum,final String link) {
+		
+		
+		super.signIn("inventor1", "inventor1");
+	
+		super.clickOnMenu("Inventor", "List patronages");
+		//show
+		super.clickOnListingRecord(recordIndex);
+		super.clickOnButton("Create Report");
+		super.fillInputBoxIn("memorandum", "Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia nonLorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, aerat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non");
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+	}
 
 
 	// Ancillary methods ------------------------------------------------------
