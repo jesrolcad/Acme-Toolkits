d3ae9ffd423bfbacbb86544ad0288dfef7748f4b
jesrolcad <jesrolcad@alum.us.es>
2022-05-23 13:42:55 +0200

    Task 013: Fixing tests

--- src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
@@ -21,10 +21,10 @@ public class PatronPatronageCreateTest extends TestHarness{
     
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/patron/patronage/create.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/patron/patronage/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(1)
 	public void positivePatronage(final int recordIndex, final String code, final String budget, final String start_date, final String end_date, 
-		final String status, final String legal_stuff, final String link,final String inventor_username, final String inventor_company, final String inventor_link, final String inventor_statement, final String inventorId) {
+		final String status, final String legal_stuff, final String link, final String inventorId) {
 		
 		
 		super.signIn("patron1", "patron1");
@@ -36,14 +36,14 @@ public class PatronPatronageCreateTest extends TestHarness{
 
 		super.fillInputBoxIn("code", code);
 		
-		super.fillInputBoxIn("startDate",this.formattedDateTimeStart );
+		super.fillInputBoxIn("startDate",this.formattedDateTimeStart);
 		super.fillInputBoxIn("endDate", this.formattedDateTimeEND);
 		super.fillInputBoxIn("legalStuff", legal_stuff);
 		super.fillInputBoxIn("budget", budget);
 		super.fillInputBoxIn("link", link);
 		final BrowserDriver driver = super.getDriver();
 		driver.locateOne(By.xpath("//*[@id=\"inventorId\"]/option[" + inventorId +"]")).click();
-		super.clickOnSubmit("Confirm creating new patronage");
+		super.clickOnSubmit("Create patronage");
 		super.clickOnMenu("Patron", "List patronages");
 
 		super.sortListing(0, "asc");
@@ -53,13 +53,14 @@ public class PatronPatronageCreateTest extends TestHarness{
 		super.checkInputBoxHasValue("endDate", this.formattedDateTimeEND);
 		super.checkInputBoxHasValue("legalStuff", legal_stuff);
 		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("status", status);
 		super.checkInputBoxHasValue("link", link);
 
 		
 		super.signOut();
 	}
 	@ParameterizedTest
-	@CsvFileSource(resources = "/patron/patronage/negativeCreateNull.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/patron/patronage/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(2)
 	public void negativePatronage(final int recordIndex, final String code, final String budget, final String start_date, final String end_date, 
 		final String status, final String legal_stuff, final String link,final String inventor_username, final String inventor_company, final String inventor_link, final String inventor_statement) {
@@ -80,7 +81,7 @@ public class PatronPatronageCreateTest extends TestHarness{
 		super.fillInputBoxIn("budget", budget);
 		super.fillInputBoxIn("link", link);
 		
-		super.clickOnSubmit("Confirm creating new patronage");
+		super.clickOnSubmit("Create patronage");
 		super.checkErrorsExist();
 		super.signOut();
 	}
--- src/test/java/acme/testing/patron/patronage/PatronPatronageDeleteTest.java
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageDeleteTest.java
@@ -22,22 +22,63 @@ public class PatronPatronageDeleteTest extends TestHarness{
     
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/patron/patronage/delete.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(1)
+	@CsvFileSource(resources = "/patron/patronage/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
 	public void positivePatronage(final int recordIndex, final String code, final String budget, final String start_date, final String end_date, 
 		final String status, final String legal_stuff, final String link,final String inventor_username, final String inventor_company, final String inventor_link, final String inventor_statement) {
 		
 		
-		super.signIn("patron1", "patron1");
+		super.signIn("patron4", "patron4");
 		//list
 		super.clickOnMenu("Patron", "List patronages");
-//		Integer prev=this.repository.tests();
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.checkColumnHasValue(recordIndex, 1, budget);
+		super.checkColumnHasValue(recordIndex, 2, start_date);
+		super.checkColumnHasValue(recordIndex, 3, end_date);
+		super.checkColumnHasValue(recordIndex, 4, status);
 		super.clickOnListingRecord(recordIndex);
-		super.clickOnSubmit("Delete patronage(and associated reports)");
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("startDate", start_date);
+		super.checkInputBoxHasValue("endDate", end_date);
+		super.checkInputBoxHasValue("legalStuff", legal_stuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("status", status);
+		super.checkInputBoxHasValue("optionalLink", link);
+		super.clickOnSubmit("Delete patronage");
+		super.checkNotErrorsExist();
+		
 		super.clickOnMenu("Patron", "List patronages");
 		super.checkNotErrorsExist();
-//		assert prev.equals(this.repository.tests());
 
+		
+		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativePatronage(final int recordIndex, final String code, final String budget, final String start_date, final String end_date, 
+		final String status, final String legal_stuff, final String link,final String inventor_username, final String inventor_company, final String inventor_link, final String inventor_statement) {
+		
+		
+		super.signIn("patron4", "patron4");
+		//list
+		super.clickOnMenu("Patron", "List patronages");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("startDate", start_date);
+		super.checkInputBoxHasValue("endDate", end_date);
+		super.checkInputBoxHasValue("legalStuff", legal_stuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("status", status);
+		super.checkInputBoxHasValue("optionalLink", link);
+		super.checkNotSubmitExists("Delete patronage");
 		
 		super.signOut();
 	}
